0ff70fa059932824993d533104fbb0cdcaef4e93
lena <evkhodosova@gmail.com>
2021-12-13 12:42:32 +0300

    add check_places function

--- src/functions.sql
+++ src/functions.sql
@@ -1,3 +1,14 @@
+create or replace function check_places(чемодан integer, ид_локации integer) returns integer as $$
+declare
+    animals integer;
+    size integer;
+begin
+    select count(ид_зверек) into animals from зверьки_в_чемодане where зверьки_в_чемодане.ид_чемодан = чемодан;
+    select размер into size from локация where локация.ид_локация = ид_локации;
+    return size - animals * 5;
+end;
+$$ language plpgsql;
+
 create or replace procedure entrollment_laboratory(ид_лаборатория integer, ид_зверек integer, дата_время timestamp) as $$
 begin
     insert into запись_лаборатория(ид_лаборатория, ид_зверек, дата_время) values(ид_лаборатория, ид_зверек, дата_время);
